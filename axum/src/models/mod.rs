c875a90f1ad9840c1322577987e2d66c8bed15cd
psw <agnusdei1207@gmail.com>
2025-04-19 10:05:27 +0900

    refactoring

--- axum/src/models/mod.rs
+++ axum/src/models/mod.rs
@@ -1,2 +1,1 @@
-pub mod common;
 pub mod eve_json_log;
--- axum/target/debug/deps/axum-c3c6eb4a34255150.d
+++ axum/target/debug/deps/axum-c3c6eb4a34255150.d
@@ -1,12 +1,11 @@
-/Users/agnusdei/workspace/bastion/axum/target/debug/deps/libaxum-c3c6eb4a34255150.rmeta: src/main.rs src/handlers/mod.rs src/handlers/eve_json_log.rs src/models/mod.rs src/models/common.rs src/models/eve_json_log.rs src/routes/mod.rs src/routes/eve_json_log.rs src/cors/mod.rs src/cors/cors.rs
+/Users/agnusdei/workspace/bastion/axum/target/debug/deps/libaxum-c3c6eb4a34255150.rmeta: src/main.rs src/handlers/mod.rs src/handlers/eve_json_log.rs src/models/mod.rs src/models/eve_json_log.rs src/routes/mod.rs src/routes/eve_json_log.rs src/cors/mod.rs src/cors/cors.rs
 
-/Users/agnusdei/workspace/bastion/axum/target/debug/deps/axum-c3c6eb4a34255150.d: src/main.rs src/handlers/mod.rs src/handlers/eve_json_log.rs src/models/mod.rs src/models/common.rs src/models/eve_json_log.rs src/routes/mod.rs src/routes/eve_json_log.rs src/cors/mod.rs src/cors/cors.rs
+/Users/agnusdei/workspace/bastion/axum/target/debug/deps/axum-c3c6eb4a34255150.d: src/main.rs src/handlers/mod.rs src/handlers/eve_json_log.rs src/models/mod.rs src/models/eve_json_log.rs src/routes/mod.rs src/routes/eve_json_log.rs src/cors/mod.rs src/cors/cors.rs
 
 src/main.rs:
 src/handlers/mod.rs:
 src/handlers/eve_json_log.rs:
 src/models/mod.rs:
-src/models/common.rs:
 src/models/eve_json_log.rs:
 src/routes/mod.rs:
 src/routes/eve_json_log.rs:
