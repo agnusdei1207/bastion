e63498f7cf36eb952b4fbf2e01b39cb4c60ffff4
psw <agnusdei1207@gmail.com>
2025-04-20 11:02:06 +0900

    fix error route {id}

--- axum/src/routes/rule.rs
+++ axum/src/routes/rule.rs
@@ -8,11 +8,11 @@ use crate::handlers::rule::{create_rule, delete_rule, get_rule, get_rules};
 pub fn router_rule() -> Router {
     Router::new()
         .nest(
-            "/rules", 
+            "/rule", 
             Router::new()
                 .route("/", get(get_rules))
-                .route("/:id", get(get_rule))
+                .route("/{id}", get(get_rule))
                 .route("/", post(create_rule))
-                .route("/:id", delete(delete_rule))
+                .route("/{id}", delete(delete_rule))
         )
 }
